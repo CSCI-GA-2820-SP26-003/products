1c44e191a00fbb829d3d6d6534c0587aae669742
Zhen Yang <markt0815@icloud.com>
2026-02-21 01:28:41 +0000

    added Product model

--- tests/test_models.py
+++ tests/test_models.py
@@ -23,8 +23,8 @@ import os
 import logging
 from unittest import TestCase
 from wsgi import app
-from service.models import YourResourceModel, DataValidationError, db
-from .factories import YourResourceModelFactory
+from service.models import Product, DataValidationError, db
+from .factories import ProductFactory
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
@@ -32,11 +32,11 @@ DATABASE_URI = os.getenv(
 
 
 ######################################################################
-#  YourResourceModel   M O D E L   T E S T   C A S E S
+#  Product   M O D E L   T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceModel(TestCase):
-    """Test Cases for YourResourceModel Model"""
+class TestProduct(TestCase):
+    """Test Cases for Product Model"""
 
     @classmethod
     def setUpClass(cls):
@@ -54,7 +54,7 @@ class TestYourResourceModel(TestCase):
 
     def setUp(self):
         """This runs before each test"""
-        db.session.query(YourResourceModel).delete()  # clean up the last tests
+        db.session.query(Product).delete()  # clean up the last tests
         db.session.commit()
 
     def tearDown(self):
@@ -65,15 +65,29 @@ class TestYourResourceModel(TestCase):
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_example_replace_this(self):
-        """It should create a YourResourceModel"""
-        # Todo: Remove this test case example
-        resource = YourResourceModelFactory()
-        resource.create()
-        self.assertIsNotNone(resource.id)
-        found = YourResourceModel.all()
+    # def test_example_replace_this(self):
+    #     """It should create a Product"""
+    #     # Todo: Remove this test case example
+    #     resource = ProductFactory()
+    #     resource.create()
+    #     self.assertIsNotNone(resource.id)
+    #     found = Product.all()
+    #     self.assertEqual(len(found), 1)
+    #     data = Product.find(resource.id)
+    #     self.assertEqual(data.name, resource.name)
+
+    def test_create_account(self):
+        """It should create a Product"""
+        product = ProductFactory()
+        product.create()
+        self.assertIsNotNone(product.id)
+        found = Product.all()
         self.assertEqual(len(found), 1)
-        data = YourResourceModel.find(resource.id)
-        self.assertEqual(data.name, resource.name)
+        data = Product.find(product.id)
+        self.assertEqual(data.name, product.name)
+        self.assertEqual(data.price, product.price)
+        self.assertEqual(data.description, product.description)
+        self.assertEqual(data.image_url, product.image_url)
+        self.assertEqual(data.sku, product.sku)
 
     # Todo: Add your test cases here...
