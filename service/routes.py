17b1d6d2910cbad599c639b7a768cc80a7ed2b22
PJ Farrell <pf2290@nyu.edu>
2026-03-09 22:55:33 -0400

    Added Patch route. Reverted PUT route to expect full data object. Removed duplicate tests. Updated tests that should be for PATCH not PUT.

--- service/routes.py
+++ service/routes.py
@@ -131,10 +131,36 @@ def get_products(product_id):
 
 
 ######################################################################
-# UPDATE A NEW PRODUCT
+# UPDATE A NEW PRODUCT PUT AND PATCH
 ######################################################################
 @app.route("/products/<int:product_id>", methods=["PUT"])
 def update_products(product_id):
+    """
+    Update a single product
+
+    This endpoint will return whether it was successful or not
+    """
+    app.logger.info("Request to Update a product with id [%s]", product_id)
+
+    # Attempt to find the Product and abort if not found
+    product = Product.find(product_id)
+    if not product:
+        abort(
+            status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found."
+        )
+
+    # Update the Product with the data in the request
+    check_content_type("application/json")
+    data = request.get_json()
+    app.logger.info("Processing: %s", data)
+    product.deserialize(data)
+    product.update()
+    app.logger.info("Product with id [%s] updated!", product.id)
+    return jsonify(product.serialize()), status.HTTP_200_OK
+
+
+@app.route("/products/<int:product_id>", methods=["PATCH"])
+def patch_products(product_id):
     """
     Update a single product
     Returns 200 if the update is successful
